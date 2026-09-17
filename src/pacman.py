e6b47fa397843535aeaa09a1217be3bdaf94a939
dzm1337 <diogodefhyy@gmail.com>
2026-09-17 21:20:30 +0200

    Feat: added the Maze class in the constructor method

--- src/pacman.py
+++ src/pacman.py
@@ -1,27 +1,24 @@
 from src.config import Config
-
-
+from src.maze import Maze
 
 
 class Pacman:
-
-    def __init__(self, config: Config):
+    def __init__(self, maze: Maze, config: Config) -> None:
+        self.maze = Maze(config)
         self.lives: int = config.lives
         self.pp_pacgum: int = config.points_per_pacgum
         self.pp_superpacgum: int = config.point_per_superpacgum
         self.pp_ghost: int = config.points_per_ghost
         self.total_pacgums: int = 0
         self.total_superpacgums: int = 0
+        self.height = maze.height
+        self.width = maze.width
 
 
-    # We need the central coordinates to define where the pacman should spawn. -> return tuple[int, int]
-#    def spawn_pacman() -> tuple[int, int]:
-
-    # Function to define if the pacman is able to move to X direction. -> return bool
 #    def able_to_move() -> bool:
 
-    # Function to check if pacman coordinates is the same as ghosts.
+# Function to check if pacman coordinates is the same as ghosts.
 #    def check_collision() -> bool:
 
-    # Keep track of pacgums and superpacgums ingested by the pacman.
+# Keep track of pacgums and superpacgums ingested by the pacman.
 #    def ingested_gums() -> None:
